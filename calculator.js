0b82402610b290e5c1fb53bb0cc81cf43e0ce758
Peter John Reyes <peter.john.reyes0326@gmail.com>
2023-09-25 16:56:38 +0800

    bmi calculator with post

---
+++ calculator.js
@@ -0,0 +1,34 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", (req, res) =>{
+  let height = Number(req.body.height);
+  let weight = Number(req.body.weight);
+
+  let result = weight/(height ** 2);
+
+  if (result <= 18.5){
+    res.send(`You are Under weight, your BMI is ${result}`)
+  }
+  else if(result >= 18.5 && result <= 24.9){
+    res.send(`You are Normal weight, your BMI is ${result}`)
+  }
+  else if(result >= 25 && result <= 29.9){
+    res.send(`You are Over weight, your BMI is ${result}`)
+  }
+  else if(result >= 30){
+    res.send(`You are over weight, your BMI is ${result}`)
+  }
+  
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
